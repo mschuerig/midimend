76a494434380aa8ed3b78e85621e4bb86263d022
Michael Schürig <michael@schuerig.de>
2026-07-13 02:11:15 +0200

    Bump version to 0.2.1

--- Sources/midimend-cli/main.swift
+++ Sources/midimend-cli/main.swift
@@ -9,7 +9,7 @@ setlinebuf(stdout)
 let arguments = Array(CommandLine.arguments.dropFirst())
 
 /// Bumped per release; the release workflow refuses tags that don't match.
-let midimendVersion = "0.2.0"
+let midimendVersion = "0.2.1"
 
 func printUsage() {
     print("""
